177a737ec195c1632e928547911c2c6ec7617215
Ilya Volkov <illvolkov@bk.ru>
2022-09-24 02:29:39 +0500

    Create model of data

---
+++ AutoRuAppPrototype/Sources/GarageModule/Model/Promotion.swift
@@ -0,0 +1,13 @@
+//
+//  Promotion.swift
+//  AutoRuAppPrototype
+//
+//  Created by Ilya Volkov on 23.09.2022.
+//
+
+struct Promotion {
+    let image: String
+    let subImage: String?
+    let title: String
+    let subTitle: String?
+}
---
+++ AutoRuAppPrototype/Sources/GarageModule/Model/PromotionSectionType.swift
@@ -0,0 +1,12 @@
+//
+//  PromotionSectionType.swift
+//  AutoRuAppPrototype
+//
+//  Created by Ilya Volkov on 23.09.2022.
+//
+
+enum PromotionSectionType {
+    case promoWithButton
+    case discount
+    case promo
+}
